d025e487502144c7ac447b26100d06d657d1933c
Mohd Talib <96992202+Mohd1999-collb@users.noreply.github.com>
2022-05-24 00:40:23 +0530

    Add files via upload

---
+++ Js/game/canvas.js
@@ -0,0 +1,17 @@
+Game.Canvas = {
+  create: function(width, height) {
+    return this.init($({ tag: 'canvas'}), width, height);
+  },
+  init: function(canvas, width, height) {
+    canvas = $(canvas);
+    canvas.width = width;
+    canvas.height = height;
+    return canvas;
+  },
+  render: function(width, height, render, canvas) {
+    canvas = canvas || this.create(width, height);
+    render(canvas.getContext('2d'), width, height);
+    return canvas;
+  }
+
+};
